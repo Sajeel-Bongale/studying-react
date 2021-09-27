a7d11df47d9e64bad96112d5bc15668d26b7d47b
Sajeel-Bongale <sajeelbongale@gmail.com>
2021-09-27 09:55:55 +0530

    Minor Updates

--- studing-react/001.getting-started/001.what-is-react.js
+++ studing-react/001.getting-started/001.what-is-react.js
@@ -5,21 +5,21 @@
 
     As against old applications which were required to make a new request to fetch updated pages
     every time an update was required, JS allows the user to make smaller requests to fetch
-    only that change parts of the page.
+    only those parts of the page that need changing.
 
     JavaScript in the browser is able to manipulate to so-called Dom, the HTML elements
-    that are rendered onto the screen. We can manipulate this with JavaScript.
-    And that allows us to change what the users sees without fetching a new HTML page.
+    that are rendered onto the screen. And that allows us to change what the users sees without
+    fetching a new HTML page.
 
     However the entirety of this functionality is provided by JS and it can be achieved without the
     need for ReactJS.
 
-    React powers websites to be fluid, responsive and Reactive. It makes the app look and feel like
+    React powers websites to be fluid, responsive and reactive. It makes the app look and feel like
     a mobile app with smooth transitions and highly reactive user experience.
 
     React is all about splitting your application into small building blocks, small components,
     where every building block, every component, has a clear task and therefore your code
-    stays maintainable and manageable and React, the library, will do the heavy lifting
+    stays maintainable and manageable. React, the library, will do the heavy lifting
     of rendering something on the screen and of combining all your code.
 
     It helps simplify instructions which end up changing and reacting to the events in the DOM
